e2604afcc717eb91d16fc9d3ca11da9d9e68c040
laith faris <laithabuali37@gmail.com>
2021-12-04 21:04:24 +0200

    update user table adding some coulmn

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->integer("nid")->unique();
+            $table->string('full_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer("age");
             $table->rememberToken();
             $table->timestamps();
         });
